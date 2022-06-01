25fe36f99d1c5411841af4c6fd8c771e61fa9806
radhika <desineniradhika@gmail.com>
2022-05-31 23:06:23 -0400

    adding script to my samplebash

---
+++ samplebash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir /tmp/samplebash_folder1
+touch /tmp/samplebash_folder1/myfirstbashfile1.txt
+echo "adding text using my first bash script" > /tmp/samplebash_folder1/myfirstbashfile1.txt
+
